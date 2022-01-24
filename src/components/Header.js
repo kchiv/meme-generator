ab341e3c98abdbceac3a812a3302f0a166cea098
Kyle Chivers <chiverskyle@gmail.com>
2022-01-23 16:44:45 -0800

    Added more styling to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 function Header() {
   return (
-    <header>
-      <h1>Meme Generator</h1>
-    </header>
+    <header className="header">
+            <img 
+                src="./images/troll-face.png" 
+                className="header--image"
+            />
+            <h2 className="header--title">Header component</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
   );
 }
 
