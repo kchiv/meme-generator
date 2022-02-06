7176ee6cef9dc8967bc806a65153f6017baf76a1
Kyle Chivers <chiverskyle@gmail.com>
2022-02-06 14:42:41 -0800

    created getMemeImage function
    Created function that gets the "url" key for each object within the memesData.js array. This function currently just logs a random URL from the array each time the form button is clicked.

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,6 +1,14 @@
 import React from "react";
+import memesData from '../memesData.js';
 
 function Meme() {
+    function getMemeImage() {
+        const memesArray = memesData.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length);
+        const url = memesArray[randomNumber].url;
+        console.log(url);
+    }
+
     return (
         <main>
             <div className="form">
@@ -16,6 +24,7 @@ function Meme() {
                 />
                 <button 
                     className="form--button"
+                    onClick={getMemeImage}
                 >
                     Get a new meme image 🖼
                 </button>
